80b9fbd98d745e6f23396e75aa4e1b63c1a49a9b
Bruno Tavares <connect+github@bltavares.com>
2020-01-17 23:22:33 -0300

    Make it compile in windows

--- colmeia-mdns/src/lib.rs
+++ colmeia-mdns/src/lib.rs
@@ -33,11 +33,22 @@ fn bind_multicast(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
     socket.bind(&socket2::SockAddr::from(*addr))
 }
 
+/// Only Unix has this setup
+#[cfg(unix)]
+fn reuse_port(socket: &Socket) -> io::Result<()> {
+    socket.set_reuse_port(true)
+}
+
+#[cfg(windows)]
+fn reuse_port(_socket: &Socket) -> io::Result<()> {
+    Ok(())
+}
+
 pub fn create_shared_socket() -> Result<UdpSocket, io::Error> {
     let socket = Socket::new(Domain::ipv4(), Type::dgram(), Some(Protocol::udp()))?;
     socket.set_read_timeout(Some(Duration::from_millis(100)))?;
     socket.set_reuse_address(true)?;
-    socket.set_reuse_port(true)?;
+    reuse_port(&socket)?;
     socket.join_multicast_v4(&IPV4_MULTICAST, &Ipv4Addr::UNSPECIFIED)?;
     bind_multicast(&socket, &DAT_MULTICAST)?;
     Ok(socket.into_udp_socket())
--- src/bin/colmeia-mdns.rs
+++ src/bin/colmeia-mdns.rs
@@ -1,11 +1,11 @@
+use colmeia_mdns::Locator;
+use futures::stream::StreamExt;
+use std::time::Duration;
+
 fn name() -> String {
   let args: Vec<String> = std::env::args().skip(1).collect();
   args.first().expect("must have dat name").into()
 }
-
-use colmeia_mdns::Locator;
-use futures::stream::StreamExt;
-use std::time::Duration;
 
 fn main() {
   let socket = colmeia_mdns::create_shared_socket().expect("socket creation failed");
