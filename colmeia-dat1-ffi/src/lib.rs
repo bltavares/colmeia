66fc77fdb390f2a4143d4471522ca86dd985edfd
Bruno Tavares <connect+github@bltavares.com>
2020-02-09 09:16:37 -0300

    Initial FFI tests
    I would like to test if it is possible to ship a embedded Dat engine
    into a mobile app.
    
    Talking to friends, I've learned that iOS does not ship with any
    `exec`-like API, so it would not be viable to ship a binary there.
    
    After taking a look on Dart FFI, I'm considering testing it directly
    with FFI.
    The effort to build a Flutter + binary application seems to be on the
    same league as pushing a Flutter + ffi sample.
    
    This test intends to do the following:
    
    - [ ] Exec a hardcoded sync from Flutter Android
    - [ ] Validate isolate FFI in the background
    - [ ] Test it on desktop, if possible.
    - [ ] Test on the iOS simulator (and maybe an iPhone if I can find a volunteer)
    
    The steps of evolving this sample would be, IMO:
    
    - Sync a hardcoded dat
    - Sync a dat url passed over FFI, each on an isolate
    - Port [hyperdrive-daemon](https://github.com/andrewosh/hyperdrive-daemon) and embed a server into the app, to avoid FFI method calls

---
+++ colmeia-dat1-ffi/src/lib.rs
@@ -0,0 +1,16 @@
+#[no_mangle]
+pub extern "C" fn sync() {
+    env_logger::init();
+
+    let key = "dat://642b2da5e4267635259152eb0b1c04416030a891acd65d6c942b8227b8cbabed";
+    let dat_key = colmeia_dat1_core::parse(&key).expect("invalid dat argument");
+    let dat_key = match dat_key {
+        colmeia_dat1_core::DatUrlResolution::HashUrl(result) => result,
+        _ => panic!("invalid hash key"),
+    };
+
+    let mut dat = colmeia_dat1::Dat::in_memory(dat_key, "0.0.0.0:3899".parse().unwrap());
+    dat.with_discovery(vec![dat.lan()]);
+
+    async_std::task::block_on(dat.sync());
+}
