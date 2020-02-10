947ddbe26338376a1c3ed853c48caa9f856f22fc
Bruno Tavares <connect+github@bltavares.com>
2020-02-10 14:44:56 -0300

    Avoid unwindws across FFI, and log panics on error

--- colmeia-dat1-ffi/src/lib.rs
+++ colmeia-dat1-ffi/src/lib.rs
@@ -5,10 +5,18 @@ fn logging() {
     android_logger::init_once(Config::default().with_min_level(Level::Debug));
 }
 
+use futures::future::FutureExt;
+
 #[no_mangle]
 pub extern "C" fn sync() {
-    logging();
+    use std::panic;
 
+    panic::set_hook(Box::new(|e| {
+        log::error!("Error: {:?}", e);
+        println!("Custom panic hook");
+    }));
+
+    logging();
     let key = "dat://642b2da5e4267635259152eb0b1c04416030a891acd65d6c942b8227b8cbabed";
     let dat_key = colmeia_dat1_core::parse(&key).expect("invalid dat argument");
     let dat_key = match dat_key {
@@ -18,5 +26,10 @@ pub extern "C" fn sync() {
 
     let mut dat = colmeia_dat1::Dat::in_memory(dat_key, "0.0.0.0:43898".parse().unwrap());
     dat.with_discovery(vec![dat.lan()]);
-    async_std::task::spawn(dat.sync());
+    log::warn!("Startig");
+    async_std::task::spawn(async {
+        let stream = std::panic::AssertUnwindSafe(dat.sync());
+        let result = stream.catch_unwind().await;
+        log::error!("Error {:?}", result);
+    });
 }
