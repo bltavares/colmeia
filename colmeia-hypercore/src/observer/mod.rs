f184c75751ab898f02b6d735d8b394a47e69059f
Bruno Tavares <connect+github@bltavares.com>
2020-07-15 22:21:21 -0300

    Fix fmt

--- colmeia-hypercore/src/observer/mod.rs
+++ colmeia-hypercore/src/observer/mod.rs
@@ -2,4 +2,4 @@ mod event;
 mod message;
 
 pub use event::{EventDriver, EventObserver};
-pub use message::{MessageDriver, MessageObserver, Emit};
+pub use message::{Emit, MessageDriver, MessageObserver};
