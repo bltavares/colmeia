cddf82f7253d6d14ba6c0105a98694a94d659e9a
Bruno Tavares <connect+github@bltavares.com>
2020-07-26 10:38:29 -0300

    Avoid exposing things with * on lib

--- colmeia-hypercore/src/lib.rs
+++ colmeia-hypercore/src/lib.rs
@@ -4,7 +4,7 @@ mod network;
 mod schema;
 mod utils;
 
-pub use hyperdrive::*;
-pub use hyperstack::*;
-pub use network::*;
-pub use utils::*;
+pub use hyperdrive::{in_memmory, Hyperdrive};
+pub use hyperstack::Hyperstack;
+pub use network::{replicate_channel, replicate_hyperdrive, Emit};
+pub use utils::{HashParserError, PublicKeyExt};
